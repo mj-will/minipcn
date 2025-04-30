9c85519abe3348b5783da7cd16f4c1caa40a92ec
Michael J. Williams <michaeljw1@googlemail.com>
2025-04-30 14:16:50 +0100

    TST: add basic sampling test

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def rng():
+    """Fixture to provide a random number generator."""
+    return np.random.default_rng(seed=42)
+
+
+@pytest.fixture
+def log_target_fn():
+    """Fixture to provide a log target function."""
+
+    def _log_target_fn(x):
+        return -0.5 * np.sum(x**2, axis=-1)
+
+    return _log_target_fn
---
+++ tests/integration_tests/test_sampling.py
@@ -0,0 +1,19 @@
+from minicrank import Sampler
+from minicrank.step import TCPNStep
+
+
+def test_sampling_with_tcpn(rng, log_target_fn):
+    dims = 4
+    x_init = rng.normal(size=(100, dims))  # Initial samples
+
+    sampler = Sampler(
+        log_prob_fn=log_target_fn,
+        dims=dims,
+        step_fn=TCPNStep(dims=dims, rng=rng, rho=0.5),
+        rng=rng,
+        target_acceptance_rate=0.234,
+    )
+
+    chain, history = sampler.sample(x_init, n_steps=100)
+    assert chain.shape == (101, 100, 4)
+    assert history.it[-1] == 99
