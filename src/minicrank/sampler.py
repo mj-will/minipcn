b8a3e10e474222f18109df9eb590da447743ba9b
mj-will <michaeljw1@googlemail.com>
2025-05-01 13:13:17 +0100

    ENH: improve progress bar

--- src/minicrank/sampler.py
+++ src/minicrank/sampler.py
@@ -67,25 +67,33 @@ class Sampler:
         chain = np.empty((n_steps + 1, x.shape[0], x.shape[1]), dtype=x.dtype)
         chain[0] = x
         states = []
-        for i in trange(n_steps):
-            x_new, log_jac = self.step_fn(x)
-            log_prob_x_new = self.log_prob_fn(x_new)
-            log_alpha = log_prob_x_new + log_jac - log_prob_x
-            alpha = np.exp(np.minimum(0, log_alpha))  # Shape: (N,)
+        with trange(n_steps, desc="Sampling", unit="step") as pbar:
+            for i in pbar:
+                x_new, log_jac = self.step_fn(x)
+                log_prob_x_new = self.log_prob_fn(x_new)
+                log_alpha = log_prob_x_new + log_jac - log_prob_x
+                alpha = np.exp(np.minimum(0, log_alpha))  # Shape: (N,)
 
-            accept = self.rng.uniform(size=len(x_new)) < alpha
-            x = np.where(accept[:, None], x_new, x)  # Shape: (N, D)
-            log_prob_x = np.where(accept, log_prob_x_new, log_prob_x)
-            chain[i + 1] = x
+                accept = self.rng.uniform(size=len(x_new)) < alpha
+                x = np.where(accept[:, None], x_new, x)  # Shape: (N, D)
+                log_prob_x = np.where(accept, log_prob_x_new, log_prob_x)
+                chain[i + 1] = x
 
-            state = ChainState(
-                it=i,
-                acceptance_rate=float(np.mean(accept)),
-                target_acceptance_rate=self.target_acceptance_rate,
-            )
-            self.step_fn.update(state)
-            state = self.step_fn.update_state(state)
-            states.append(state)
+                state = ChainState(
+                    it=i,
+                    acceptance_rate=float(np.mean(accept)),
+                    target_acceptance_rate=self.target_acceptance_rate,
+                )
+                self.step_fn.update(state)
+                state = self.step_fn.update_state(state)
+                states.append(state)
+                pbar_dict = {
+                    "acceptance_rate": state.acceptance_rate,
+                }
+                pbar_dict.update(
+                    state.extra_stats
+                )
+                pbar.set_postfix(pbar_dict)
 
         state_history = ChainStateHistory.from_chain_states(states)
         return chain, state_history
